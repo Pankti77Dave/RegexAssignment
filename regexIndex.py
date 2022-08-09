fb3a24f0ed2896686cf82cfa0138f5b10a34f89d
pankti77 <pankti77dave@gmail.com>
2022-08-09 10:24:28 +0530

    did minor changes

--- regexIndex.py
+++ regexIndex.py
@@ -1,47 +1,46 @@
 import re
 import urllib.request
 
-f = urllib.request.urlopen("file:///D:/regex_assign/test.html")
+f = urllib.request.urlopen("file:///D:/regex_assign/regexIndexHTML.html")
 text= f.read()
 text = text.decode()
 
-appointmentReference = re.findall(r"([A-Z]{2}\d{8}-\d{5})", text)
-print(appointmentReference)
-
-patientName = re.findall(r"([A-Za-z]{3,}\s[A-Za-z]{3,})", text)
-print(patientName)
-
-patientPhone = re.findall(r"(\(\d{3}\)\s\d{3}-\d{4})", text)
-print(patientPhone)
-
-agentNameOne = re.findall(r"(>[A-Za-z]{6}\s[A-Za-z]{5,})", text)
-print(agentNameOne)
-agentNameTwo = re.findall(r"(N\/A)", text)
-print(agentNameTwo)
-
-agentName = agentNameOne + agentNameTwo
-
-
-appointmentStatus = re.findall(r"([A-Z]{9})", text)
-print(appointmentStatus)
-
-trackingIdOne = re.findall(r"(>\s\d{5,})", text)
-print(trackingIdOne)
-
-trackingIdTwo = re.findall(r"(\sn\/a)", text)
-print(trackingIdTwo)
-
-trackingId = trackingIdOne + trackingIdTwo
-
-appointmentDAndT = re.findall(r"(\d{2}\s\w{3}\s\d{4}\s\d{2}:\d{2}\s[A-Z]{2})", text)
-print(appointmentDAndT)
-
-
-# Python code to create a file
+trSelector = re.findall("<tr>(\s*<td.{1,}\s*.{1,}\s*.{1,}\s*<td.{1,}\s*<td.{1,}\s*.{1,}\s*<td.{1,}\s*<td.{1,}\s*<td.{1,}\s*<td.{1,}\s*<td.{1,}\s*)\s*<\/tr>", text)
+# print(trSelector)
+row = " "
 file = open('RegexAssignment.txt','w')
-data = " "
-for i in range(0,34):
-    data = appointmentReference[i] + " " + patientName[i] + " " + agentName[i] + " " + appointmentStatus[i] + " " +  trackingId[i] + " " + appointmentDAndT[i] + "\n"
-    file.write(data)
-    data = " "
-file.close()
+for i in trSelector:
+    appointmentReference = re.findall(r"([A-Z]{2}\d{8}-\d{5})", i)
+    row += appointmentReference[0] + " "
+
+    patientName = re.findall(r"(\">[A-Za-z]{3,}\s[\s]?[A-Za-z]{3,})", i)
+
+    patientAddress = re.findall(r"(\w{1,}\s*\w{1,}\s\w{1,},\s)?(\w{1,}\s)?(\w{1,},\s)?(\w(1,)\s)?(\w{1,}\s)?(\w{1,}\s)?(\w{1,}\s\w{1,},\s)?\w{1,},\s*(\s*|\w{1,})\s\w{1,},\s\w{1,},.\d{5}", i)
+    str = ''
+    for item in patientAddress:
+        for address in item:
+            str = str + address
+    patientPhone = re.findall(r"(\(\d{3}\)\s\d{3}-\d{4})", i)
+
+    appointmentStatus = re.findall(r"([A-Z]{9})", i) 
+
+    row += patientName[0] + " " + str + " " + patientPhone[0] + " " 
+    if len(patientName) == 2:
+        row += patientName[1] + " "
+    else:
+        row += "N\A" + " "
+    row += appointmentStatus[0] + " "
+
+    trackingIdOne = re.findall(r"(>\s\d{5,})", i)
+
+    if len(trackingIdOne) == 0:
+        row += "n\a" + " "
+    else:
+        row += trackingIdOne[0] + " "
+
+    appointmentDAndT = re.findall(r"(\d{2}\s\w{3}\s\d{4}\s\d{2}:\d{2}\s[A-Z]{2})", i)
+    row += appointmentDAndT[0] + "\n"
+    file.write(row)
+    print(row)
+    row = " "
+    
